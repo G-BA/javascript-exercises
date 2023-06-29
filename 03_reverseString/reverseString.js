6a658ec080627a60c03e07319b4c5e119ebac7b0
G-BA <geo.anderson13@gmail.com>
2023-06-29 15:27:42 +0100

    Complete e3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(input_string) {
+    let container = Array.from(input_string);
+    container = container.reverse();
+    return container.toString().replaceAll(',','');
 };
 
 // Do not edit below this line
