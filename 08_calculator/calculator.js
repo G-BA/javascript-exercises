4130e58a98b15692f8f5685af315d6a0b0ce2a28
G-BA <geo.anderson13@gmail.com>
2023-06-30 12:34:50 +0100

    JS exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
 const add = function() {
+  return arguments [0] + arguments [1];
 	
 };
 
 const subtract = function() {
+  return arguments [0] - arguments [1];
 	
 };
 
-const sum = function() {
-	
+const sum = function(input_array) {
+  let result = 0;
+  for(let i = 0; i < input_array.length; i++){
+    result += input_array[i];
+  }
+  return result;
 };
 
 const multiply = function() {
+  let result = 1;
+  for(let i = 0; i < arguments.length;i++){
+    result *= arguments[i];
+  }
+  return result;
 
 };
 
-const power = function() {
-	
+const power = function(number1, number2) {
+    return number1 ** number2;
 };
 
 const factorial = function() {
-	
+  let result = 1;
+  for(let i = 1; i < arguments[0]+1;i++){
+    result = result * i;
+};
+return result;
 };
 
 // Do not edit below this line
