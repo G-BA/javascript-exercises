ce4ef84291cc554a93a01dfafb23613593085b89
G-BA <geo.anderson13@gmail.com>
2023-06-30 11:53:28 +0100

    Compelete E6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,8 @@
 const leapYears = function() {
-
-};
+    if(arguments[0]%4 == 0 && arguments[0]%100 != 0){return true;}
+    else if(arguments[0]%4 == 0 && arguments[0]%100 == 0 && arguments[0]%400 == 0){return true}
+    else{return false}
+    };
 
 // Do not edit below this line
 module.exports = leapYears;
