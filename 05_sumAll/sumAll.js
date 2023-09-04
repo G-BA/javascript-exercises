700a54b61bb3067df005896ff5d5df094ad0ece6
G-BA <geo.anderson13@gmail.com>
2023-09-04 17:02:19 +0100

    getting everything up to date after a break

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
 const sumAll = function() {
 
+    if(arguments[0] < 0 || arguments[0] < 0){return "ERROR"};
+    if(isNaN(arguments[0]) == false || isNaN(arguments[0] == false) ){return "ERROR"};
+    
+    let result = 0;
+    for(i = arguments[0]; i <= arguments[1]; i++){
+        result += i;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
